16d07f0668eb51e0cd3a8c9a2989b83194da8a1f
viniciusandradee <vini.aclopes@gmail.com>
2024-11-16 12:45:59 -0300

    Initial commit

---
+++ src/main/java/com/SolarTracker/SolarTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.SolarTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SolarTracker/SolarTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SolarTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
